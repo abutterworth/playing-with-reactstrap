e032b09da6ba570685729d6706046a6066a476ba
Adam Butterworth <abutterworth@edx.org>
2019-01-16 14:10:39 -0500

    added some stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import ExampleForm from './ExampleForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container className="app">
+        <Row>
+          <Col>
+            <h1>Playing with Reactstrap</h1>
+            
+          </Col>
+        </Row>
+        <Row className="demo">
+          <Col>.col</Col>
+        </Row>
+        <Row className="demo">
+          <Col>.col</Col>
+          <Col>.col</Col>
+          <Col>.col</Col>
+          <Col>.col</Col>
+        </Row>
+        <Row className="demo">
+          <Col xs="3">.col-3</Col>
+          <Col xs="auto">.col-auto - variable width content</Col>
+          <Col xs="3">.col-3</Col>
+        </Row>
+        <Row className="demo">
+          <Col xs="6">.col-6</Col>
+          <Col xs="6">.col-6</Col>
+        </Row>
+        <Row className="demo">
+          <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
+          <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
+          <Col sm="4">.col-sm-4</Col>
+        </Row>
+        <Row className="demo">
+          <Col sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .order-sm-2 .offset-sm-1</Col>
+        </Row>
+        <Row className="demo">
+          <Col sm="12" md={{ size: 6, offset: 3 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
+        </Row>
+        <Row className="demo">
+          <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
+          <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
+        </Row>
+        <Row className="mt-5">
+          <Col sm="3" className="bg-secondary" style={{position:'relative'}}>
+            <div style={{position:'sticky', top:'1rem'}}> 
+              <h3>Some helper text</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum metus quam, accumsan et lectus id, condimentum convallis augue.</p>
+            </div>
+
+
+          </Col>
+          <Col sm="6">
+            <Row>
+              <Col className="justify-content-end d-flex">
+              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} className="">
+                <DropdownToggle caret>
+                  Dropdown
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem header>Header</DropdownItem>
+                  <DropdownItem>Some Action</DropdownItem>
+                  <DropdownItem disabled>Action (disabled)</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Foo Action</DropdownItem>
+                  <DropdownItem>Bar Action</DropdownItem>
+                  <DropdownItem>Quo Action</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+              </Col>
+            </Row>
+            <ExampleForm />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
---
+++ src/ExampleForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText, FormFeedback, Row } from 'reactstrap';
+
+export default class ExampleForm extends React.Component {
+  render() {
+    return (
+      <Form className="pb-5">
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input invalid />
+          <FormFeedback>Oh noes! that name is already taken</FormFeedback>
+          <FormText>Example help text that remains unchanged.</FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleSelect">Select</Label>
+          <Input type="select" name="select" id="exampleSelect">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleSelectMulti">Select Multiple</Label>
+          <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">Text Area</Label>
+          <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleFile">File</Label>
+          <Input type="file" name="file" id="exampleFile" />
+          <FormText color="muted">
+            This is some placeholder block-level help text for the above input.
+            It's a bit lighter and easily wraps to a new line.
+          </FormText>
+        </FormGroup>
+        <FormGroup tag="fieldset">
+          <legend>Radio Buttons</legend>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="radio1" />{' '}
+              Option one is this and that—be sure to include why it's great
+            </Label>
+            <Label check>
+              <Input type="radio" name="radio1" />{' '}
+              Option two can be something else and selecting it will deselect option one
+            </Label>
+            <Label check>
+              <Input type="radio" name="radio1" disabled />{' '}
+              Option three is disabled
+            </Label>
+          </FormGroup>
+        </FormGroup>
+        <FormGroup >
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" />{' '}
+              Check me out
+            </Label>
+          </FormGroup>
+        </FormGroup>
+        <Row form className="justify-content-end">
+          <Button color="primary" outline>Cancel</Button> <Button className="ml-2" color="primary">Submit</Button>
+        </Row>
+      </Form>
+    );
+  }
+}
